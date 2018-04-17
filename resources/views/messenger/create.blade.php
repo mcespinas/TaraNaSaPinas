e0ef8d0a38e546feb873f02470c4df136bb78318
mcespinas <32087489+mcespinas@users.noreply.github.com>
2018-04-18 04:32:50 +0800

    messaging system

--- resources/views/messenger/create.blade.php
+++ resources/views/messenger/create.blade.php
@@ -5,6 +5,19 @@
     <form action="{{ route('messages.store') }}" method="post">
         {{ csrf_field() }}
         <div class="col-md-6">
+
+            <!--Recipient Form Input -->
+            <div class="form-group">
+                    <label class="control-label"> Recipients: </label>
+                @if($users->count() > 0)
+                    <div class="checkbox">
+                        @foreach($users as $user)
+                            <label title="{{ $user->name }}"><input type="checkbox" name="recipients[]"
+                                                                    value="{{ $user->id }}" checked>{!!$user->name!!}</label>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
             <!-- Subject Form Input -->
             <div class="form-group">
                 <label class="control-label">Subject</label>
@@ -17,15 +30,6 @@
                 <label class="control-label">Message</label>
                 <textarea name="message" class="form-control">{{ old('message') }}</textarea>
             </div>
-
-            @if($users->count() > 0)
-                <div class="checkbox">
-                    @foreach($users as $user)
-                        <label title="{{ $user->name }}"><input type="checkbox" name="recipients[]"
-                                                                value="{{ $user->id }}">{!!$user->name!!}</label>
-                    @endforeach
-                </div>
-            @endif
     
             <!-- Submit Form Input -->
             <div class="form-group">
